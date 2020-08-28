ab189d029ad2607adb266ee3a44fa0448b98b3f5
hoda <hhussin@trudoc24x7.com>
2020-08-28 21:24:28 +0200

    add depth first search implementation

--- binary_tree.py
+++ binary_tree.py
@@ -33,27 +33,27 @@ class BinaryTree(object):
 
 
 ## iteratve solution
-    def insert(self, val):
-        cur = self.root
-        if not cur:
-            self.root = Node(val)
-            return self.root
-
-        while cur:
-            if cur.info > val:
-                if cur.left:
-                    cur = cur.left
-                else:
-                    cur.left = Node(val)
-                    break
-            else:
-                if cur.right:
-                    cur = cur.right
-                else:
-                    cur.right = Node(val)
-                    break
-
-        return self.root
+    # def insert(self, val):
+    #     cur = self.root
+    #     if not cur:
+    #         self.root = Node(val)
+    #         return self.root
+    #
+    #     while cur:
+    #         if cur.info > val:
+    #             if cur.left:
+    #                 cur = cur.left
+    #             else:
+    #                 cur.left = Node(val)
+    #                 break
+    #         else:
+    #             if cur.right:
+    #                 cur = cur.right
+    #             else:
+    #                 cur.right = Node(val)
+    #                 break
+    #
+    #     return self.root
 
 
     def traverse_min(self):
